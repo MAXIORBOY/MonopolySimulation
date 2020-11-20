f43f8dec13b345cdd17e9b8011e30218fbfcf681
MAXIORBOY <michal.grzegorz.sobieraj@gmail.com>
2020-11-20 02:52:22 +0100

    Complete code refactoring

--- Main.py
+++ Main.py
@@ -4,180 +4,176 @@ import numpy as np
 import math as mth
 
 
-def generate_cards():
-    cards = []
-    for i in range(16):
-        cards.append(i+1)
+class Cards:
+    def __init__(self, number_of_cards):
+        self.number_of_cards = number_of_cards
+        self.deck_order = self.generate_cards()
+        self.shuffle_deck()
+        self.card_index = 0
 
-    rnd.shuffle(cards)
+    def generate_cards(self):
+        return [i + 1 for i in range(self.number_of_cards)]
 
-    return cards
+    def shuffle_deck(self):
+        rnd.shuffle(self.deck_order)
 
+    def increment_card_index(self):
+        self.card_index += 1
+        if self.card_index > self.number_of_cards - 1:
+            self.card_index = 0
 
-def cc_card_definition(actual_position, cc_card_value):
-    ap = actual_position
-    if cc_card_value == 1:
-        ap = 0
-
-    elif cc_card_value == 2:
-        ap = 10
-
-    return ap
-
-
-def ch_card_definition(actual_position, ch_card_value):
-    ap = actual_position
-
-    def one():
-        return 0
-
-    def two():
-        return 10
-
-    def three():
-        return 11
-
-    def four():
-        return 24
-
-    def five():
-        return 39
-
-    def six():
-        return 5
-
-    def seven_eight():
-        if 5 < actual_position < 15:
-            return 15
-        elif 15 < actual_position < 25:
-            return 25
-        elif 25 < actual_position < 35:
-            return 35
-        else:
-            return 5
-
-    def nine():
-        if 12 < actual_position < 28:
-            return 28
-        else:
-            return 12
-
-    def ten(pos):
-        return pos - 3
-
-    switcher = {1: one(), 2: two(), 3: three(), 4: four(), 5: five(), 6: six(), 7: seven_eight(), 8: seven_eight(), 9: nine(), 10: ten(ap)}
-
-    if ch_card_value < 11:
-        ap = switcher.get(ch_card_value)
-
-    return ap
+    def reset_card_index(self):
+        self.card_index = 0
 
 
-def throw_dice(sides=6):
-    dice1 = rnd.randint(1, sides)
-    dice2 = rnd.randint(1, sides)
+class CommunityChestCards(Cards):
+    def __init__(self):
+        super().__init__(number_of_cards=17)
+        self.cards_pick_up_locations = [3, 18, 34]
+        self.card_definitions = self.define_cards()
 
-    double = dice1 == dice2
-
-    return dice1 + dice2, double
-
-
-def simulation(turns=100, games=100000):  # simulation options
-    cc_locations = [2, 17, 33]
-    ch_locations = [7, 22, 36]
-    squares_all_games = [0] * 40
-
-    for g in range(games):
-        squares_one_game = [0] * 40
-        actual_position = 0
-        double_this_turn = False
-        doubles = 0
-        cc_cards = generate_cards()
-        ch_cards = generate_cards()
-        cc_cards_counter = 0
-        ch_cards_counter = 0
-
-        for t in range(turns):
-            if not double_this_turn:
-                doubles = 0
-            position_advance, double_this_turn = throw_dice()
-            if double_this_turn:
-                doubles += 1
-            if doubles == 3:
-                actual_position = 10
-            else:
-                actual_position += position_advance
-                if actual_position > 39:
-                    actual_position -= 40
-
-            if actual_position == 30:
-                actual_position = 10
-
-            elif actual_position in ch_locations:
-                actual_position = ch_card_definition(actual_position, ch_cards[ch_cards_counter])
-                ch_cards_counter += 1
-                if ch_cards_counter > 15:
-                    ch_cards_counter = 0
-
-            elif actual_position in cc_locations:
-                actual_position = cc_card_definition(actual_position, cc_cards[cc_cards_counter])
-                cc_cards_counter += 1
-                if cc_cards_counter > 15:
-                    cc_cards_counter = 0
-
-            squares_one_game[actual_position] += 1
-
-        for i in range(len(squares_all_games)):
-            squares_all_games[i] += squares_one_game[i]
-
-    sum_of_all = sum(squares_all_games)
-    for i in range(len(squares_all_games)):
-        squares_all_games[i] = round(100 * squares_all_games[i] / sum_of_all, 2)
-
-    return squares_all_games
+    @staticmethod
+    def define_cards():
+        return {1: {'set': 1},
+                6: {'set': 11}}
 
 
-def convert_list_to_matrix(square_list):
-    rows = []
-    for i in range(11):
-        if i == 0:
-            rows.append(square_list[0:11])
-        elif i < 10:
-            row = [square_list[-i]]
-            row.extend([0.00] * 9)
-            row.extend([square_list[10 + i]])
-            rows.append(row)
-        else:
-            rows.append(square_list[30:19:-1])
+class ChanceCards(Cards):
+    def __init__(self):
+        super().__init__(number_of_cards=16)
+        self.cards_pick_up_locations = [8, 23, 37]
+        self.card_definitions = self.define_cards()
 
-    return np.array(rows)
-
-
-def visualization(matrix):
-    norm_matrix = []
-
-    for i in range(len(matrix)):
-        row = []
-        for j in range(len(matrix[0])):
-            row.append(mth.log(matrix[i][j] + 1, 10))
-
-        norm_matrix.append(row)
-
-    norm_matrix = np.array(norm_matrix)
-
-    fig, ax = plt.subplots()
-
-    ax.imshow(norm_matrix, cmap='jet')
-
-    for i in range(len(norm_matrix)):
-        for j in range(len(norm_matrix)):
-            if i == 0 or i == len(norm_matrix) - 1 or j == 0 or j == len(norm_matrix) - 1:
-                ax.text(j, i, matrix[i, j], ha="center", va="center", color="k")
-
-    ax.set_title("A percentage chance for ending a turn at the particular square in the Monopoly game \n")
-    fig.tight_layout()
-    plt.axis('off')
-    plt.show()
+    @staticmethod
+    def define_cards():
+        return {1: {'set': 1},
+                2: {'set': 25},
+                3: {'set': 12},
+                4: {'nearest': [13, 29]},
+                5: {'nearest': [6, 16, 26, 36]},
+                8: {'move': -3},
+                9: {'set': 11},
+                12: {'set': 6},
+                13: {'set': 40}}
 
 
-visualization(convert_list_to_matrix(simulation()))
+class MonopolySimulation:
+    def __init__(self, turns=100, games=1000000):
+        self.turns = turns
+        self.games = games
+
+        self.community_chest_cards = CommunityChestCards()
+        self.chance_cards = ChanceCards()
+
+        self.end_turn_fields_occurrences_array = [0] * 40
+        self.percentage_matrix = np.zeros((11, 11))
+        self.run_simulation()
+        self.end_turn_fields_percentages_array = self.convert_occurrences_into_percentages()
+        self.build_matrix_from_array()
+
+    @staticmethod
+    def throw_dice(sides=6):
+        dice1 = rnd.randint(1, sides)
+        dice2 = rnd.randint(1, sides)
+
+        double = dice1 == dice2
+
+        return dice1 + dice2, double
+
+    def register_end_turn_field(self, field):
+        self.end_turn_fields_occurrences_array[field - 1] += 1
+
+    @staticmethod
+    def bring_position_value_back_to_range(position):
+        if position < 1:
+            position += 40
+
+        elif position > 40:
+            position -= 40
+
+        return position
+
+    def change_current_position_based_on_card(self, movement_instruction, current_position):
+        movement_type, movement_value = list(movement_instruction.items())[0]
+        if movement_type == 'set':
+            return movement_value
+
+        elif movement_type == 'nearest':
+            distance_array = [self.bring_position_value_back_to_range(value - current_position) for value in movement_value]
+            return movement_value[distance_array.index(min(distance_array))]
+
+        elif movement_type == 'move':
+            return self.bring_position_value_back_to_range(current_position + movement_value)
+
+    def run_simulation(self):
+        for game in range(self.games):
+            current_position = 1
+            doubles = 0
+            for turn in range(self.turns):
+                going_to_jail = False
+                dice_value, is_double = self.throw_dice()
+                doubles += int(is_double)
+
+                if is_double:
+                    doubles += 1
+                    if doubles == 3:
+                        doubles = 0
+                        current_position = 11
+                        going_to_jail = True
+                else:
+                    doubles = 0
+
+                if not going_to_jail:
+                    current_position += dice_value
+                    current_position = self.bring_position_value_back_to_range(current_position)
+                    if current_position == 31:
+                        current_position = 11
+                    else:
+                        if current_position in self.chance_cards.cards_pick_up_locations:
+                            movement_instruction = self.chance_cards.card_definitions.get(self.chance_cards.deck_order[self.chance_cards.card_index], None)
+                            self.chance_cards.increment_card_index()
+                            if movement_instruction is not None:
+                                current_position = self.change_current_position_based_on_card(movement_instruction, current_position)
+
+                        if current_position in self.community_chest_cards.cards_pick_up_locations:
+                            movement_instruction = self.community_chest_cards.card_definitions.get(self.community_chest_cards.deck_order[self.community_chest_cards.card_index], None)
+                            self.community_chest_cards.increment_card_index()
+                            if movement_instruction is not None:
+                                current_position = self.change_current_position_based_on_card(movement_instruction, current_position)
+
+                self.register_end_turn_field(current_position)
+
+            self.community_chest_cards.shuffle_deck()
+            self.community_chest_cards.reset_card_index()
+            self.chance_cards.shuffle_deck()
+            self.chance_cards.reset_card_index()
+
+    def convert_occurrences_into_percentages(self):
+        sum_of_all_end_turn_fields = sum(self.end_turn_fields_occurrences_array)
+        return [100 * self.end_turn_fields_occurrences_array[i] / sum_of_all_end_turn_fields for i in range(len(self.end_turn_fields_occurrences_array))]
+
+    def build_matrix_from_array(self):
+        self.percentage_matrix[0, 0:-1] = self.end_turn_fields_percentages_array[0: 10]
+        self.percentage_matrix[0:-1, -1] = self.end_turn_fields_percentages_array[10: 20]
+        self.percentage_matrix[-1, 1:] = list(reversed(self.end_turn_fields_percentages_array[20: 30]))
+        self.percentage_matrix[1:, 0] = list(reversed(self.end_turn_fields_percentages_array[30: 40]))
+
+    def transform_percentage_matrix(self):
+        return np.array([[mth.log(self.percentage_matrix[i, j] + 1, 10) for j in range(len(self.percentage_matrix[i]))] for i in range(len(self.percentage_matrix))])
+
+    def view_results(self):
+        fig, ax = plt.subplots()
+        ax.imshow(self.transform_percentage_matrix(), cmap='jet')
+
+        for i in range(len(self.percentage_matrix)):
+            for j in range(len(self.percentage_matrix)):
+                if i == 0 or i == len(self.percentage_matrix) - 1 or j == 0 or j == len(self.percentage_matrix) - 1:
+                    if i != 0 or j != 0:
+                        ax.text(j, i, f'{round(self.percentage_matrix[i, j], 2)}%', ha="center", va="center", color="k")
+                    else:
+                        ax.text(j, i, f'START -> \n{round(self.percentage_matrix[i, j], 2)}%', ha="center", va="center", color="k")
+
+        ax.set_title("A percentage chance for ending a turn at the particular square in the Monopoly game \n")
+        fig.tight_layout()
+        plt.axis('off')
+        plt.show()
